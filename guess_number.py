973dea8ef06d84be6c49a5272d6a5c18eac92c7c
Piske <164142209+XXPiske@users.noreply.github.com>
2026-05-20 00:35:49 +0300

    v.1.1.0
    + separate function
    - double cycle

--- guess_number.py
+++ guess_number.py
@@ -1,17 +1,24 @@
 import random
 
+def get_int_from_input(user_input: str) -> int | None:
+    try:
+        user_number = int(user_input)
+        return user_number
+    except ValueError:
+        return None
+
 def main():
     the_chosen_number = random.randint(1,100)
     while True:
-        while True:
-            try:
-                user_number = int(input('Take a guess 1 to 100: '))
-                if user_number < 1 or user_number > 100:
-                    print('Error: Out of range! Use number from 1 to 100')
-                    continue
-                break
-            except ValueError:
-                print('Error: Use integer!')
+        user_input = input('Take a guess 1 to 100: ')
+        user_number = get_int_from_input(user_input)
+        if user_number is None:
+            print('Error: Use integer!')
+            continue
+        elif user_number < 1 or user_number > 100:
+            print('Error: Out of range! Use number from 1 to 100')
+            continue
+        
         if user_number == the_chosen_number:
             print(f"You won! The number was: {the_chosen_number}")
             break
