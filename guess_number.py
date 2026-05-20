49724f13d72005157a22b2671c3870c4fac563a1
Piske <164142209+XXPiske@users.noreply.github.com>
2026-05-20 11:32:41 +0300

    v1.3.0
    + restart game

--- guess_number.py
+++ guess_number.py
@@ -7,7 +7,7 @@ def get_int_from_input(user_input: str) -> int | None:
     except ValueError:
         return None
 
-def set_range():
+def set_range() -> tuple[int, int]:
     while True:
         left_bound = input('Set left guessing bound: ')
         left_int_bound = get_int_from_input(left_bound)
@@ -30,26 +30,40 @@ def set_range():
                 break
     return left_int_bound, right_int_bound
 
-def main():
-    left_int_bound, right_int_bound = set_range()
-    the_chosen_number = random.randint(left_int_bound,right_int_bound)
+def restart() -> bool:
     while True:
-        user_input = input(f'Take a guess from {left_int_bound} to {right_int_bound}: ')
-        user_number = get_int_from_input(user_input)
-        if user_number is None:
-            print('Error: Use integer!')
-            continue
-        elif user_number < left_int_bound or user_number > right_int_bound:
-            print(f'Error: Out of range! Use number from {left_int_bound} to {right_int_bound}')
-            continue
+        ask = input("Type 'y' to start again and 'n' to end the game: ")
+        if ask == 'y':
+            return True
+        if ask == 'n':
+            return False
+
+def main():
+    while True:
+        left_int_bound, right_int_bound = set_range()
+        the_chosen_number = random.randint(left_int_bound,right_int_bound)
+        while True:
+            user_input = input(f'Take a guess from {left_int_bound} to {right_int_bound}: ')
+            user_number = get_int_from_input(user_input)
+            if user_number is None:
+                print('Error: Use integer!')
+                continue
+            elif user_number < left_int_bound or user_number > right_int_bound:
+                print(f'Error: Out of range! Use number from {left_int_bound} to {right_int_bound}')
+                continue
+            
+            if user_number == the_chosen_number:
+                print(f"You won! The number was: {the_chosen_number}")
+                break
+            elif user_number > the_chosen_number:
+                print('Smaller')
+            else:
+                print('Bigger')
         
-        if user_number == the_chosen_number:
-            print(f"You won! The number was: {the_chosen_number}")
-            break
-        elif user_number > the_chosen_number:
-            print('Smaller')
+        if restart():
+            continue
         else:
-            print('Bigger')
+            break
    
 if __name__ == '__main__':
     main()
